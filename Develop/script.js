b06a77fe30b711d1cd9397da969b2d57814b83ad
cgarnica92 <c_garnica92@yahoo.com>
2022-08-02 15:33:54 -0400

    charset choices coded, doesn't work

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+var charOptions = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz","0123456789","!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+
 var generatePassword = function() {
   var passwordLength = window.prompt("Password Length:\nEnter a Value Between 8 and 128");
 
@@ -9,7 +11,8 @@ var generatePassword = function() {
   if (passwordLength < 8 && passwordLength > 128) {
      
   } else if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please Enter a Value Between 8 and 128")
+    window.alert("Please Enter a Value Between 8 and 128");
+    return;
   }
 
   var upCase = window.confirm("Add Upper Case Characters?");
@@ -18,7 +21,42 @@ var generatePassword = function() {
 
   var numVal = window.confirm("Add Numbers?");
 
-  var spChar = window.confirm("Add Speacial Characters?");
+  var spChar = window.confirm("Add Special Characters? \ne.g. !$@#&...");
+  
+  if (upCase && lowCase && numVal && spChar) {
+    var characters = (charOptions);
+  } else if (upCase && lowCase && numVal && spChar === false) {
+    var characters = (charOptions[0] + charOptions[1] + charOptions[2]);
+  } else if (upCase && lowCase && numVal === false && spChar === false) {
+    var characters = (charOptions[0] + charOptions[1]);
+  } else if (upCase && lowCase === false && numVal === false && spChar === false) {
+    var characters = (charOptions[0]);
+  } else if (upCase === false && lowCase && numVal && spChar === false) {
+    var characters = (charOptions[1] + charOptions[2]);
+  } else if (upCase === false && lowCase === false && numVal === false && spChar) {
+    var characters = (charOptions[3]);
+  } else if (upCase === false && lowCase === false && numVal && spChar === false) {
+    var characters = (charOptions[2]);
+  } else if (upCase === false && lowCase && numVal === false && spChar === false) {
+    var characters = (charOptions[1]);
+  } else if (upCase === false && lowCase && numVal && spChar) {
+    var characters = (charOptions[1] + charOptions[2] + charOptions[3]);
+  } else if (upCase === false && lowCase === false && numVal && spChar) {
+    var characters = (charOptions[2] + charOptions[3]);
+  } else if (upCase === false && lowCase && numVal === false && spChar) {
+    var characters = (charOptions[1] + charOptions[3]);
+  } else if (upCase && lowCase === false && numVal && spChar === false) {
+    var characters = (charOptions[0] + charOptions[2]);
+  } else if (upCase && lowCase === false && numVal === false && spChar) {
+    var characters = (charOptions[0] + charOptions[3]);
+  } else {
+    return;
+  }
+
+  for(var i= 0; i = passwordLength; i++) {
+    var characters[i];
+    console.log(characters[i]);
+  }
 }
 
 // Get references to the #generate element
